e2f8c3a49b2201826411c6e511f29cad3ca88374
GKovtonyuk <112175748+GKovtonyuk@users.noreply.github.com>
2026-05-31 21:45:45 +0300

    Update solids.js

--- js/solids.js
+++ js/solids.js
@@ -1,9 +1,17 @@
-function createGeometry(type){
-switch(type){
-case 'Tetrahedron': return new THREE.TetrahedronGeometry(1);
-case 'Cube': return new THREE.BoxGeometry(1,1,1);
-case 'Octahedron': return new THREE.OctahedronGeometry(1);
-case 'Dodecahedron': return new THREE.DodecahedronGeometry(1);
-case 'Icosahedron': return new THREE.IcosahedronGeometry(1);
-}
+const descriptions = {
+  Tetrahedron: "🔺 Тетраедр — 4 трикутні грані.",
+  Cube: "🟦 Куб — 6 квадратних граней.",
+  Octahedron: "🔶 Октаедр — 8 трикутних граней.",
+  Dodecahedron: "⬟ Додекаедр — 12 п’ятикутних граней.",
+  Icosahedron: "🔷 Ікосаедр — 20 трикутних граней."
+};
+
+function createGeometry(type) {
+  switch (type) {
+    case "Tetrahedron": return new THREE.TetrahedronGeometry(1);
+    case "Cube": return new THREE.BoxGeometry(1,1,1);
+    case "Octahedron": return new THREE.OctahedronGeometry(1);
+    case "Dodecahedron": return new THREE.DodecahedronGeometry(1);
+    case "Icosahedron": return new THREE.IcosahedronGeometry(1);
+  }
 }
